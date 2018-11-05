ea1b8a612f7d7940c4a0d5cac22e4c5cbb21b40c
Simon Tucker <simon@brotherlogic@gmail>
2018-11-04 20:01:35 -0800

    Faster updates

--- recordsalesutils.go
+++ recordsalesutils.go
@@ -40,7 +40,7 @@ func (s *Server) syncSales(ctx context.Context) {
 func (s *Server) updateSales(ctx context.Context) {
 	s.updates++
 	for _, sale := range s.config.Sales {
-		if time.Now().Sub(time.Unix(sale.LastUpdateTime, 0)) > time.Hour*24 { //one week
+		if time.Now().Sub(time.Unix(sale.LastUpdateTime, 0)) > time.Minute*30 { //one week
 			s.RaiseIssue(ctx, "Updating Sale Price", fmt.Sprintf("Updating price of %v", sale.InstanceId), false)
 		}
 	}
