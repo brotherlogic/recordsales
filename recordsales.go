1946e4bea11a4a4b6e8563d9ebb419907deed722
Simon Tucker <simon@brotherlogic@gmail>
2018-11-07 15:19:39 -0800

    Adds time tracking

--- recordsales.go
+++ recordsales.go
@@ -135,6 +135,7 @@ func (s *Server) GetState() []*pbg.State {
 		&pbg.State{Key: "active_sales", Value: int64(len(s.config.Sales))},
 		&pbg.State{Key: "updates", Value: s.updates},
 		&pbg.State{Key: "sum_sales", Value: int64(sum)},
+		&pbg.State{Key: "last_update", TimeValue: int64(s.config.Sales[0].LastUpdateTime)},
 	}
 }
 
