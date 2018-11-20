0fc4b623f4e2ff322ab0f5f9ab54587c9ca0ed54
Simon Tucker <simon@brotherlogic@gmail>
2018-11-20 08:07:42 -0800

    Adds sale archives to the mix. This closes #23

--- recordsales.go
+++ recordsales.go
@@ -137,6 +137,7 @@ func (s *Server) GetState() []*pbg.State {
 		&pbg.State{Key: "sum_sales", Value: int64(sum)},
 		&pbg.State{Key: "last_update", TimeValue: int64(s.config.Sales[0].LastUpdateTime)},
 		&pbg.State{Key: "last_price", Value: int64(s.config.Sales[0].Price)},
+		&pbg.State{Key: "archive_sales", Value: int64(len(s.config.Archives))},
 	}
 }
 
