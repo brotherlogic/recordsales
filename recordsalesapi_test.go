a7c2fef30221490a8e92d16004ac1c7a659e4222
Simon Tucker <simon@brotherlogic@gmail>
2019-05-04 13:41:23 -0700

    Adds the test

--- recordsalesapi_test.go
+++ recordsalesapi_test.go
@@ -21,3 +21,18 @@ func TestListStale(t *testing.T) {
 		t.Errorf("Bad stales: %v", resp)
 	}
 }
+
+func TestGetState(t *testing.T) {
+	s := getTestServer()
+	s.config.Sales = append(s.config.Sales, &pb.Sale{InstanceId: 12, LastUpdateTime: time.Now().Add(time.Hour * -48).Unix()})
+	s.config.Sales = append(s.config.Sales, &pb.Sale{InstanceId: 13, LastUpdateTime: time.Now().Add(time.Hour * -5).Unix()})
+
+	resp, err := s.GetSaleState(context.Background(), &pb.GetStateRequest{InstanceId: 12})
+	if err != nil {
+		t.Errorf("Problem with getting stale: %v", err)
+	}
+
+	if len(resp.GetSales()) != 1 {
+		t.Errorf("Bad get sales pull: %v", resp)
+	}
+}
